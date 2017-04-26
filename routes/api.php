e89604811e128b5280c0770ab0f16fdd46f5b0db
cheyuwu <frank890417@gmail.com>
2017-04-26 15:11:48 +0800

    update cors twice

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Http\Request;
 //     return $request->user();
 // });
 
-Route::group(['middleware'=>"cors"],function(){
+Route::group(['prefix' => 'api', 'middleware' => ['cors', 'api'], 'namespace' => 'API'],function(){
   Route::get("/image","ApiController@json_image");
 
   Route::get("/image/search/{name}","ApiController@search_image");
